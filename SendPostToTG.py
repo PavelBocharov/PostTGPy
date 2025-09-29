98e6783151c08bfde3830ba1f24d6ca6cf45ebe5
Pavel <Pavel_Bocharov@epam.com>
2025-09-29 09:38:36 +0300

    Parallel send

--- SendPostToTG.py
+++ SendPostToTG.py
@@ -2,6 +2,7 @@ import asyncio
 import logging
 import os
 import time
+from multiprocessing import Pool
 
 import schedule
 from dotenv import load_dotenv
@@ -46,26 +47,29 @@ def __create_caption(root_path: str, img_path: str, const_text: str):
         return "<code>" + title + "</code> \n\n" + tags + "\n" + const_text
 
 
+def __work(img: str):
+    print("Start work with: %s" % img)
+    img_with_wm = FileUtils.add_watermark(ROOT_DIR, img, WATERMARK)
+    print("Add watermark: %s" % img_with_wm)
+    try:
+        asyncio.run(TelegramUtils.send_post(
+            bot_token=BOT_TOKEN,
+            chat_id=CHAT_ID,
+            image_path=img_with_wm,
+            caption=__create_caption(ROOT_DIR, img, CONST_TEXT),
+            parse_mode="HTML"
+        ))
+        os.remove(img)
+    except Exception as e:
+        print("Error, but need work. Img: %s Error: %s" % (img, e))
+        os.replace(img, img + ".error")
+    finally:
+        os.remove(img_with_wm)
+
+
 def job():
-    # imgs = FileUtils.get_list_images(root_dir=ROOT_DIR)
-    for img in FileUtils.get_list_images(root_dir=ROOT_DIR):
-        print("Start work with: %s" %img)
-        img_with_wm = FileUtils.add_watermark(ROOT_DIR, img, WATERMARK)
-        print("Add watermark: %s" %img_with_wm)
-        try:
-            asyncio.run(TelegramUtils.send_post(
-                bot_token=BOT_TOKEN,
-                chat_id=CHAT_ID,
-                image_path=img_with_wm,
-                caption=__create_caption(ROOT_DIR, img, CONST_TEXT),
-                parse_mode="HTML"
-            ))
-            os.remove(img)
-        except Exception as e:
-            print("Error, but need work. Img: %s Error: %s" % (img, e))
-            os.replace(img, img + ".error")
-        finally:
-            os.remove(img_with_wm)
+    p = Pool()
+    p.map(__work, FileUtils.get_list_images(root_dir=ROOT_DIR))
 
 
 if __name__ == '__main__':
